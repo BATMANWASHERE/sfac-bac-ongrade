52a88ad26b8cc985da93596dccdc76e3ddecf616
Jason Casas <jason.carolino.casas@gmail.com>
2022-02-11 08:54:02 +0800

    first commit

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php
+
+
+	$server = 'localhost';
+	$username = 'u733437513_03132804Jaypog';
+	$password = 'C5;ev2HZ[8v';
+	$conn = 'u733437513_enrollment';
+
+	$db = new mysqli($server, $username, $password, $conn);
+
+	date_default_timezone_set('Asia/Manila');
+
+?>
